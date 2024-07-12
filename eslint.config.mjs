a9e95177ced59ec068fd0070e787258a28bca3ad
MengLinMaker <menglinmaker@gmail.com>
2024-07-12 21:59:18 +1000

    DX: Add browser compatability check

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import compat from 'eslint-plugin-compat'
 
 export default [
   {
     ignores: ['coverage', 'dist', 'node_modules'],
   },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
+  compat.configs['flat/recommended'],
 ]
